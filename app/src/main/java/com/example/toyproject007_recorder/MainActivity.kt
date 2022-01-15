d3ed09d0c6ae4874f16e86ecbdd554f39f20d8c6
kimjinsub <hikm9985@naver.com>
2022-01-15 19:34:39 +0900

    사용자 권한 요청

--- app/src/main/java/com/example/toyproject007_recorder/MainActivity.kt
+++ app/src/main/java/com/example/toyproject007_recorder/MainActivity.kt
@@ -1,22 +1,53 @@
 package com.example.toyproject007_recorder
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
-    private val recordButton: RecordButton  by lazy{
+    private val recordButton: RecordButton by lazy {
         findViewById(R.id.recordButton)
     }
 
+    private val requiredPermissions = arrayOf(android.Manifest.permission.RECORD_AUDIO)
     private var state = State.BEFORE_RECODING
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        requestAudioPermission()
         initViews()
     }
 
-    private fun initViews(){
+
+    //    요청된 권한이 이미 허가가 되었는지 아닌지에 대해서 오버라이드 메시지를 통해서 확인 가능
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        val audioRecordPermissionGranted = requestCode == REQUEST_RECORD_AUDIO_PERMISSION &&
+                grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
+
+        if (!audioRecordPermissionGranted) {
+            finish()
+        }
+
+    }
+
+    private fun requestAudioPermission() {
+        requestPermissions(requiredPermissions, REQUEST_RECORD_AUDIO_PERMISSION)
+    }
+
+    private fun initViews() {
         recordButton.updateIconWithState(state)
+    }
+
+    companion object {
+        private const val REQUEST_RECORD_AUDIO_PERMISSION = 201
     }
 }
