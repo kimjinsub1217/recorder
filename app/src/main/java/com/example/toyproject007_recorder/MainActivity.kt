fef5fc8f4235a28d1481bc7c94af03ea91367676
kimjinsub <hikm9985@naver.com>
2022-01-16 20:41:27 +0900

    녹음  기능구현

--- app/src/main/java/com/example/toyproject007_recorder/MainActivity.kt
+++ app/src/main/java/com/example/toyproject007_recorder/MainActivity.kt
@@ -1,18 +1,38 @@
 package com.example.toyproject007_recorder
 
 import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
+    private val resetButton: Button by lazy {
+        findViewById(R.id.resetButton)
+    }
     private val recordButton: RecordButton by lazy {
         findViewById(R.id.recordButton)
     }
 
-    private val requiredPermissions = arrayOf(android.Manifest.permission.RECORD_AUDIO)
+    private val requiredPermissions = arrayOf(
+        android.Manifest.permission.RECORD_AUDIO,
+        android.Manifest.permission.READ_EXTERNAL_STORAGE
+    )
+    private val recordingFilePath: String by lazy {
+        "${externalCacheDir?.absolutePath}/recoding.3gp"
+    }
+    private var recorder: MediaRecorder? = null
+    private var player: MediaPlayer? = null
     private var state = State.BEFORE_RECODING
+        set(value) {
+            field = value
+            resetButton.isEnabled = (value == State.AFTER_RECORDING ||
+                    value == State.ON_PLAYING)//false면 버튼이 클릭이 안됨
+            recordButton.updateIconWithState(value)
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +40,8 @@ class MainActivity : AppCompatActivity() {
 
         requestAudioPermission()
         initViews()
+        bindViews()
+        initVariables()
     }
 
 
@@ -45,6 +67,72 @@ class MainActivity : AppCompatActivity() {
 
     private fun initViews() {
         recordButton.updateIconWithState(state)
+    }
+
+    private fun bindViews() {
+        resetButton.setOnClickListener {
+            stopPlying()
+            state = State.BEFORE_RECODING
+        }
+        recordButton.setOnClickListener {
+            when (state) {
+                State.BEFORE_RECODING -> {
+                    startRecording()
+                }
+                State.ON_RECORDING -> {
+                    stopRecording()
+                }
+                State.AFTER_RECORDING -> {
+                    startPlating()
+                }
+                State.ON_PLAYING -> {
+                    stopPlying()
+                }
+            }
+        }
+    }
+
+    private  fun initVariables(){
+        state = State.BEFORE_RECODING
+    }
+
+
+    private fun startRecording() {
+        recorder = MediaRecorder()
+            .apply {
+                setAudioSource(MediaRecorder.AudioSource.MIC)
+                setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+                setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+                setOutputFile(recordingFilePath)
+                prepare()
+            }
+        recorder?.start()
+        state = State.ON_RECORDING
+    }
+
+    private fun stopRecording() {
+        recorder?.run {
+            stop()
+            release()
+        }
+        recorder = null
+        state = State.AFTER_RECORDING
+    }
+
+    private fun startPlating() {
+        player = MediaPlayer()
+            .apply {
+                setDataSource(recordingFilePath)
+                prepare()
+            }
+        player?.start()
+        state = State.ON_PLAYING
+    }
+
+    private fun stopPlying() {
+        player?.release()
+        player = null
+        state = State.AFTER_RECORDING
     }
 
     companion object {
